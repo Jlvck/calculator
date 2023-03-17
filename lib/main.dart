948a4fecf8803466dfa05bc98bd88557e07d3e16
Jlvck <jonaruthan@gmail.com>
2023-03-17 09:10:49 +0100

    New Button Key method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, library_private_types_in_public_api, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, library_private_types_in_public_api, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, unnecessary_cast
 
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
@@ -30,6 +30,24 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
   double equationFontSize = 38.0;
   double resultFontSize = 48.0;
 
+  /// "÷", 1, Colors.blue
+  Map buttonKey = {
+    "key_1": {
+      'key_text': "C" as String,
+      'key_height': 1.0 as double,
+      'key_color': Colors.redAccent as Color
+    },
+    'key_2': {
+      'key_text': "⌫" as String,
+      'key_height': 1.0 as double,
+      'key_color': Colors.blue as Color
+    },
+    'key_3': {
+      'key_text': "÷" as String,
+      'key_height': 1.0 as double,
+      'key_color': Colors.blue as Color
+    }
+  };
   buttonPressed(String buttonText) {
     setState(() {
       if (buttonText == "C") {
@@ -73,12 +91,24 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
     });
   }
 
+  List<Widget> buttonList(keyList) {
+    final List<Widget> keyList = [];
+    buttonKey.forEach((key, value) {
+      return keyList.add(setButton(
+          value['key_text'] as String,
+          value['key_height'] as double,
+          value['key_color'] as Color) as Widget);
+    });
+    return keyList as List<Widget>;
+  }
+
   Widget setButton(String buttonText, double buttonHeight, Color buttonColor) {
     return BuildButton(buttonText, buttonHeight, buttonColor, buttonPressed);
   }
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> keyList = [];
     final mediaQuery = MediaQuery.of(context);
     return Scaffold(
       appBar: AppBar(title: Text('SimpleCalculator')),
@@ -108,11 +138,7 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
                 width: mediaQuery.size.width * 0.75,
                 child: Table(
                   children: [
-                    TableRow(children: [
-                      setButton("C", 1, Colors.redAccent),
-                      setButton("⌫", 1, Colors.blue),
-                      setButton("÷", 1, Colors.blue),
-                    ]),
+                    TableRow(children: buttonList(keyList)),
                     TableRow(children: [
                       setButton("7", 1, Colors.black54),
                       setButton("8", 1, Colors.black54),
